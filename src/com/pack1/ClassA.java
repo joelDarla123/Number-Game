1bf37501feb63379a028c1745cb792b2daedb5e8
joelkumar327 <samikshait@DESKTOP-V529CMB>
2024-02-27 18:17:37 +0530

    final code

---
+++ src/com/pack1/ClassA.java
@@ -0,0 +1,52 @@
+package com.pack1;
+
+import java.util.Scanner;
+
+public class ClassA {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chances = 8;
+        int finals = 0;
+        boolean playAgain = true;
+        System.out.println("Welcome Buddy!");
+        System.out.println("Hey Buddy, you have about " + chances + " chances to win the game.");
+
+        while (playAgain) {
+            int rand = getRandomNumber(1, 1000);
+            boolean guess = false;
+
+            for (int i = 0; i < chances; i++) {
+                System.out.println("Chance " + (i + 1) + ": Enter your guess:");
+                int user = sc.nextInt();
+                if (user == rand) {
+                    guess = true;
+                    finals += 1;
+                    System.out.println("You won it!");
+                    break;
+                } else if (user > rand) {
+                    System.out.println("Too High");
+                } else {
+                    System.out.println("Too Low");
+                }
+            }
+
+            if (!guess) {
+                System.out.println("Sorry Buddy, you lost the chances. The correct number was: " + rand);
+            }
+
+            System.out.println("Do you want to play again? (y/n)");
+            String playAgainInput = sc.next();
+            playAgain = playAgainInput.equalsIgnoreCase("y");
+        }
+
+        System.out.println("That's it Buddy, hope you enjoyed it.");
+        System.out.println("Here is your score: " + finals);
+
+        sc.close();
+    }
+
+    private static int getRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+}
